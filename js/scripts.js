226a7b485f8cd5a76211fdeeb3263b92da813e62
Asia Wasilewska <asia_wasilewska@tlen.pl>
2017-05-15 12:40:13 +0200

    added function

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
-function Telefon(marka, cena, kolor) {
+function Telefon(marka, cena, kolor, aparat) {
 	this.marka = marka; 
 	this.cena = cena;
 	this.kolor = kolor;
-}
-Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".");
+  this.aparat = aparat;
 }
 
-var SamsungGalaxyS6 = new Telefon("Gal", 2000, "biały");
-var iPhone6S = new Telefon("Apple", 2250, "srebrny");
-var OnePlusOne = new Telefon("One", 5000, "złoty");
+Telefon.prototype.printInfo = function() {
+	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ". Aparat: " + this.aparat + ".");
+}
+
+var SamsungGalaxyS6 = new Telefon("Gal", 2000, "biały", "13 MPx f/1.9 AF");
+var iPhone6S = new Telefon("Apple", 2250, "srebrny", "16 MPx f/1.9");
+var OnePlusOne = new Telefon("One", 5000, "złoty", "14 MPx f/1.9");
 
 SamsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
